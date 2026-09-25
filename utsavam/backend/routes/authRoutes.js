d8348a24b52401b294d7b678a1d8cd02ae374f32
Chagantipati LOKESH <23ht1a4414@gmail.com>
2026-09-25 10:49:50 +0530

    Delete authRoutes.js file
    Remove authentication routes for user and admin.

--- utsavam/backend/routes/authRoutes.js
+++ utsavam/backend/routes/authRoutes.js
@@ -1,12 +1,1 @@
-import express from 'express'
-import { registerUser, registerAdmin, loginUser, getMe } from '../controllers/authController.js'
-import { protect } from '../middleware/authMiddleware.js'
 
-const router = express.Router()
-
-router.post('/register', registerUser)
-router.post('/register-admin', registerAdmin)
-router.post('/login', loginUser)
-router.get('/me', protect, getMe)
-
-export default router
